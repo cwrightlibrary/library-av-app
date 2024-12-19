e55c3b8128e1245f8bdf20ff0c4e07cd586e1f2d
cwrightlibrary <108359193+cwrightlibrary@users.noreply.github.com>
2024-12-19 13:57:17 -0500

    fixing the preview csv window

--- src/csv_app.py
+++ src/csv_app.py
@@ -37,23 +37,38 @@ class PreviewCSV(customtkinter.CTkToplevel):
         # Segmented button with the titles for the names of each button segment
         self.av_issues_segmented_button = customtkinter.CTkSegmentedButton(self, values=self.av_issues_titles, command=self.update_av_issues_segmented_button)
         self.av_issues_segmented_button.set(self.av_issues_titles[0])
-        self.av_issues_segmented_button.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+        self.av_issues_segmented_button.grid(row=0, column=0, padx=10, pady=10, sticky='new')
         
         # Show the CSV in a textbox
         self.textbox = customtkinter.CTkTextbox(self, font=customtkinter.CTkFont(family='Consolas'), corner_radius=4)
         self.textbox.grid(row=1, column=0, padx=10, pady=10, sticky='nsew')
         self.textbox.insert('0.0', self.av_issues_rows[0][1])
-    
-    # TODO: Update this
-    def update_av_issues_segmented_button(self):
-        current_issue = self.av_issues_segmented_button.get()
-        current_issue_index = self.av_issues_rows.index(current_issue)
-        self.textbox.delete('0.0', 'end')
-        self.textbox.insert('0.0', "\n".join(self.av_issues_rows[current_issue_index][1]))
 
+    def update_av_issues_segmented_button(self, value):
+        for c in self.av_issues_rows:
+            if c[0] == value:
+                print(c)
+                textbox_text = "\n".join(map(str, c[1]))
+        self.textbox.delete('0.0', 'end')
+        self.textbox.insert('0.0', textbox_text)
+
+
+
+class NewCSV(customtkinter.CTkToplevel):
+    def __init__(self, master, parent):
+        super().__init__(master)
+        self.parent = parent
+        
+        self.title('New CSV')
+        self.geometry('600x400')
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        
+        self.attributes('-topmost', 1)
+        self.lift()
 
 # Buttons for the App
-class OpenShowCSVFrame(customtkinter.CTkFrame):
+class OpenShowNewCSVFrame(customtkinter.CTkFrame):
     def __init__(self, master, parent):
         # Inherit the master CTkFrame properties
         super().__init__(master)
@@ -65,7 +80,7 @@ class OpenShowCSVFrame(customtkinter.CTkFrame):
         
         # Use grid system
         self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure((0, 1), weight=1)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
 
         # Load CSV button
         self.button_open_csv = customtkinter.CTkButton(
@@ -73,6 +88,11 @@ class OpenShowCSVFrame(customtkinter.CTkFrame):
         )
         # Place Load CSV button
         self.button_open_csv.grid(row=0, column=0, padx=10, pady=(10, 0), sticky='ew')
+        
+        # New CSV button
+        self.button_new_csv = customtkinter.CTkButton(self, text='New CSV', command=self.new_csv)
+        # Place New CSV button
+        self.button_new_csv.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
 
         # Preview CSV button
         self.button_preview_csv = customtkinter.CTkButton(
@@ -80,7 +100,7 @@ class OpenShowCSVFrame(customtkinter.CTkFrame):
         )
         # Place Preview CSV Button
         self.button_preview_csv.grid(
-            row=1, column=0, padx=10, pady=(10, 0), sticky='ew'
+            row=2, column=0, padx=10, pady=(10, 0), sticky='ew'
         )
 
         # If CSV file valid, disable the load button, enable the preview button
@@ -115,7 +135,9 @@ class OpenShowCSVFrame(customtkinter.CTkFrame):
     def preview_csv_window(self):
         print('Success opening CSV preview window.')
         self.preview_window = PreviewCSV(self, self.parent)
-
+    
+    def new_csv(self):
+        pass
 
 # CSV Reader App
 class AVApp(customtkinter.CTk):
@@ -140,7 +162,7 @@ class AVApp(customtkinter.CTk):
         self.grid_columnconfigure(0, weight=1)
 
         # Create a frame for the OpenShowCSVFrame
-        self.open_show_csv_frame = OpenShowCSVFrame(self, self)
+        self.open_show_csv_frame = OpenShowNewCSVFrame(self, self)
         self.open_show_csv_frame.grid(
             row=0, column=0, padx=10, pady=(10, 0), sticky='nsew'
         )
